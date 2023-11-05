8bc716c7fd9d9ebcadd9a1947734c1b8c9bb9374
Kalpesh Patil <72344646+kalpesh299@users.noreply.github.com>
2023-11-06 01:51:50 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import { Footer } from './components/Footer';
 import Header from './components/Header';
 import { Quots } from './components/Quots';
 
@@ -9,6 +10,7 @@ function App() {
       
   <Header/>
   <Quots/>
+  <Footer/>
     </div>
   );
 }
